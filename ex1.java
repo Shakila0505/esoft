10a3c28f9159dd0acf99baacc4bd19f8f2b507b9
Silva <desilvashakila655@gmail.com>
2024-10-03 14:43:29 +0530

    initial commit

--- ex1.java
+++ ex1.java
@@ -5,10 +5,3 @@ public class ex1 {
         System.out.println("Goodbye world!");
     }
 }
-public class ex1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("It's been nice knowing you");
-        System.out.println("Goodbye world!");
-    }
-}
